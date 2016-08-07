244cae2f36f620c38a124058a019c87d69da6e33
ItsJonQ <itsjonq@gmail.com>
2016-08-06 22:59:10 -0400

    Uses lodash modules instead of lodash lib

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 // Sass Pathfinder
 
 var _ = require('lodash');
+var uniq = require('lodash.uniq');
+var flattenDeep = require('lodash.flattendeep');
 
 var pathfinder = function() {
   var paths = Array.prototype.slice.call(arguments);
   paths = [].concat.apply([], paths);
 
-  return _.uniq(_.flattenDeep(paths));
+  return uniq(flattenDeep(paths));
 };
 
 module.exports = pathfinder;
